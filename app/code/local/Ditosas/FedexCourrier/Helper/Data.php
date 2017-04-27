5be7ee7bbe386dbad2141421779ca4aa052c255e
Jose David Restrepo Balbin <jose.restrepo@globant.com>
2017-04-27 14:28:20 -0500

    no message

--- app/code/local/Ditosas/FedexCourrier/Helper/Data.php
+++ app/code/local/Ditosas/FedexCourrier/Helper/Data.php
@@ -3,23 +3,35 @@
 class Ditosas_FedexCourrier_Helper_Data extends Mage_Core_Helper_Abstract{
 
     public function checkDefinedShippingCost($country_id, $region, $city){
-        $region = Mage::getModel('directory/region')->getCollection()
-            ->addFieldToFilter('country_id', array('eq' => $country_id))
-            ->addFieldToFilter('default_name', array('eq' => $region))
-            ->getFirstItem();
+        if(!$region == "" && !is_null($region)) {
+            $region = Mage::getModel('directory/region')->getCollection()
+                ->addFieldToFilter('country_id', array('eq' => $country_id))
+                ->addFieldToFilter('default_name', array('eq' => $region))
+                ->getFirstItem();
 
-        $region_id = $region->getId();
+            $region_id = $region->getId();
+            $city = Mage::getModel('ditosas_citydropdown/city')->getCollection()
+                ->addFieldToFilter('region_id', array('eq' => $region_id))
+                ->addFieldToFilter('name', array('eq' => $city))
+                ->getFirstItem();
+        } else {
+            $city = Mage::getModel('ditosas_citydropdown/city')->getCollection()
+                ->addFieldToFilter('name', array('eq' => $city))
+                ->getFirstItem();
+        }
 
-        $city = Mage::getModel('ditosas_citydropdown/city')->getCollection()
-            ->addFieldToFilter('region_id', array('eq' => $region_id))
-            ->addFieldToFilter('name', array('eq' => $city))
-            ->getFirstItem();
-
-        $model = Mage::getModel('ditosas_fedexcourrier/costos')->getCollection()
-            ->addFieldToFilter('country_id', array('eq' => $country_id))
-            ->addFieldToFilter('region_id', array('eq' => $region_id))
-            ->addFieldToFilter('city_id', array('eq' => $city->getId()))
-            ->getFirstItem();
+        if(!is_null($region) && ($region !="")) {
+            $model = Mage::getModel('ditosas_coordinadoracourrier/costos')->getCollection()
+                ->addFieldToFilter('country_id', array('eq' => $country_id))
+                ->addFieldToFilter('region_id', array('eq' => $region_id))
+                ->addFieldToFilter('city_id', array('eq' => $city->getId()))
+                ->getFirstItem();
+        } else {
+            $model = Mage::getModel('ditosas_coordinadoracourrier/costos')->getCollection()
+                ->addFieldToFilter('country_id', array('eq' => $country_id))
+                ->addFieldToFilter('city_id', array('eq' => $city->getId()))
+                ->getFirstItem();
+        }
 
         if($model)
             return $model->getCost();
