b5af2758ea258d54736cccdc7d56a883dd30aabf
Jose David Restrepo Balbin <jose.restrepo@globant.com>
2017-04-27 14:23:14 -0500

    no message

--- app/code/local/Ditosas/CoordinadoraCourrier/Helper/Data.php
+++ app/code/local/Ditosas/CoordinadoraCourrier/Helper/Data.php
@@ -3,17 +3,22 @@
 class Ditosas_CoordinadoraCourrier_Helper_Data extends Mage_Core_Helper_Abstract{
 
     public function checkDefinedShippingCost($country_id, $region, $city){
-        $region = Mage::getModel('directory/region')->getCollection()
-            ->addFieldToFilter('country_id', array('eq' => $country_id))
-            ->addFieldToFilter('default_name', array('eq' => $region))
-            ->getFirstItem();
+        if(!$region == "" && !is_null($region)) {
+            $region = Mage::getModel('directory/region')->getCollection()
+                ->addFieldToFilter('country_id', array('eq' => $country_id))
+                ->addFieldToFilter('default_name', array('eq' => $region))
+                ->getFirstItem();
 
-        $region_id = $region->getId();
-
-        $city = Mage::getModel('ditosas_citydropdown/city')->getCollection()
-            ->addFieldToFilter('region_id', array('eq' => $region_id))
-            ->addFieldToFilter('name', array('eq' => $city))
-            ->getFirstItem();
+            $region_id = $region->getId();
+            $city = Mage::getModel('ditosas_citydropdown/city')->getCollection()
+                ->addFieldToFilter('region_id', array('eq' => $region_id))
+                ->addFieldToFilter('name', array('eq' => $city))
+                ->getFirstItem();
+        } else {
+            $city = Mage::getModel('ditosas_citydropdown/city')->getCollection()
+                ->addFieldToFilter('name', array('eq' => $city))
+                ->getFirstItem();
+        }
 
         if(!is_null($region) && ($region !="")) {
             $model = Mage::getModel('ditosas_coordinadoracourrier/costos')->getCollection()
