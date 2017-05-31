8c55ddb3823ae1514727166a471fc3ae860cb272
Jose Restrepo <josda6486@gmail.com>
2017-05-31 09:52:46 -0700

    email attachments

--- app/code/core/Mage/Sales/Model/Order/Pdf/Invoice.php
+++ app/code/core/Mage/Sales/Model/Order/Pdf/Invoice.php
@@ -132,7 +132,7 @@ class Mage_Sales_Model_Order_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Abst
             /* Add document text and number */
             $this->insertDocumentNumber(
                 $page,
-                Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId()
+                Mage::helper('sales')->__('Factura # ') . $invoice->getIncrementId()
             );
             /* Add table */
             $this->_drawHeader($page);
