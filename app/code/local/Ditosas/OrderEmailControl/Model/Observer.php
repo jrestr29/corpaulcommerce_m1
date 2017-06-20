5e513609be1773c845478ef087eca5ad82ae43b0
Jose Restrepo <josda6486@gmail.com>
2017-06-20 11:35:49 -0700

    observer orders

---
+++ app/code/local/Ditosas/OrderEmailControl/Model/Observer.php
@@ -0,0 +1,27 @@
+<?php
+class Ditosas_OrderEmailControl_Model_Observer 
+{
+	public function processEmail(Varien_Event_Observer $observer)
+	{
+		$orderIds = $observer->getData('order_ids');
+
+		foreach($orderIds as $_orderId)
+		{
+			$order = Mage::getModel('sales/order')->load($_orderId);
+			$invoiceCollection = $order->getInvoiceCollection();
+
+			try {
+				foreach($invoiceCollection as $invoice) { 
+				    $invoice->sendEmail(true,'');
+				 }
+
+				$order->getSendConfirmation(null);
+				$order->sendNewOrderEmail();
+			} catch(Exception $e) {
+				var_dump($e->getMessage());
+			}
+		}
+
+		echo "Observer end";
+	}
+}
